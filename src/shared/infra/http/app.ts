b082ad55bf4fc85074ecfd68d998bd9416be7bb9
Caio Vinícius <caio1525pereira@gmail.com>
2022-09-01 20:53:19 -0300

    feat: :tada: Base do projeto
    Base do projeto e configurações iniciais

---
+++ src/shared/infra/http/app.ts
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import "dotenv/config";
+import "express-async-errors";
+import "reflect-metadata";
+
+import cors from "cors";
+import Express, { NextFunction, Request, Response } from "express";
+import swaggerUi from "swagger-ui-express";
+
+import { AppError } from "@shared/errors/AppError";
+
+import swaggerFile from "../../../swagger.json";
+import { router } from "./routes";
+
+const app = Express();
+
+app.use(cors());
+app.use(Express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use(router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+	if (err instanceof AppError) {
+		return res.status(err.statusCode).json({
+			message: err.message
+		});
+	}
+});
+
+export { app };
---
+++ src/shared/infra/http/routes/index.ts
@@ -0,0 +1,11 @@
+import { Request, Response, Router } from "express";
+
+const router = Router();
+
+router.get("/", (req: Request, res: Response) => {
+	return res.json({
+		message: "ok"
+	});
+});
+
+export { router };
